0398e0f37e70018828b2e023ddd1d5ddc6d21984
Jasleen-kaur19 <80347384+Jasleen-kaur19@users.noreply.github.com>
2024-12-12 11:43:57 -0500

    Update urls.py

--- bookstore_app/urls.py
+++ bookstore_app/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Authentication URLs
+    # User 
     path('', views.user_login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     
-    # Main Book Display and Management
+    # Book 
     path('book_list/', views.book_list, name='book_list'),
     path('add_book/', views.add_book, name='add_book'),
     path('delete-book/<int:book_id>/', views.delete_book, name='delete_book'),
     
-    # Shopping Cart Management
+    # Cart
     path('cart/', views.cart, name='cart'),
     path('get_cart_count/', views.get_cart_count, name='get_cart_count'),
     path('get_cart_items/', views.get_cart_items, name='get_cart_items'),
@@ -21,7 +21,7 @@ urlpatterns = [
     path('update-quantity/<int:book_id>/', views.update_quantity, name='update_quantity'),
     path('sync_cart/', views.sync_cart, name='sync_cart'),
     
-    # Order Processing
+    # Order 
     path('checkout/', views.checkout, name='checkout'),
     path('order/', views.order_confirmation, name='order_confirmation'),
     path('order_success/', views.order_success, name='order_success'),
